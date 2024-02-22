7ff4bfb52590478da1e11be2f1b4714a0428a0ea
Juan Sebastián Linares Torres <toliseju@gmail.com>
2024-02-22 12:42:28 -0500

    practica bootstrap

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello world!")
+k = [[5,6,7],[3,4,3]]
+
+for x in k:
+    total = 0
+    for y in x:
+        total = total + y
+    print(total)    
+
+"""
+
+
+lockers = x.split(',')
+totals = set()
+for x in lockers:
+    total = 0
+    for y in x:
+        total = total + int(y)
+    totals.add(total)
+print(max(totals))"""
+
+s = "cb"
+k = ["cd", "bd", "cccb", "bcc", "bcdcb"]
+
+correct_words = set()
+for x in k:
+    flag = 1
+    for y in x:
+        if (y in s):
+            pass
+        else:
+            flag = 0
+    if flag == 1:
+        correct_words.add(x)
+print(len(correct_words))
---
+++ test.py
@@ -0,0 +1,8 @@
+
+count = 0
+for x in range(904):
+    if x % 3 == 0:
+        print (x)
+        count += 1
+
+print(count)
