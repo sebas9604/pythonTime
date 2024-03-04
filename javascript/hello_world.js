39c839dfe5f3b3258425d652405c8e1b2f44baad
Juan Sebastián Linares Torres <toliseju@gmail.com>
2024-03-04 14:26:04 -0500

    Practicas bootstrap

---
+++ javascript/hello_world.js
@@ -0,0 +1,11 @@
+//comentario
+console.log("Hola mundo!" )
+
+//variables
+
+var myString = "Hola, Sebas" // no tiene en cuenta el scope
+console.log(myString)
+
+let myString2 = "Cadena2"
+myString2 = "valorCambiado"
+console.log(myString2)
